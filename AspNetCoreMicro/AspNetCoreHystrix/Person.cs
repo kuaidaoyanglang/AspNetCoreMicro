301b900358566330ee54dc8dafa1623ce2ff41cd
杨浪 <1042744766@qq.com>
2019-03-27 16:10:03 +0800

    Ocelot

---
+++ AspNetCoreMicro/AspNetCoreHystrix/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreHystrix
+{
+    public class Person
+    {
+        [CustomInsterceptor]
+        public virtual string Say(string msg)
+        {
+            Console.WriteLine("service calling ..." + msg);
+            return msg;
+        }
+    }
+}
---
+++ AspNetCoreMicro/AspNetCoreHystrix/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using AspectCore.Extensions.DependencyInjection;
+
+namespace AspNetCoreHystrix
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddSingleton<Person>();
+
+            RegisterServices(this.GetType().Assembly, services);
+
+            return services.BuildAspectInjectorProvider();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+
+        private void RegisterServices(Assembly asm, IServiceCollection services)
+        {
+            foreach (Type type in asm.GetExportedTypes())
+            {
+                bool hasCustomInterceptorAttr = type.GetMethods().Any(m=>m.IsDefined(typeof(CustomInsterceptorAttribute),true));
+                if (hasCustomInterceptorAttr)
+                {
+                    services.AddSingleton(type);
+                }
+            }
+        }
+    }
+}
